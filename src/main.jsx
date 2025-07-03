50153aad23853e64c14f27b786603ad42cc0c72e
Juried59 <juried59@gmail.com>
2025-07-03 12:38:41 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import PasswordGate from './PasswordGate.jsx' // ✅ Import the gate
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PasswordGate>
+      <App />
+    </PasswordGate>
+  </React.StrictMode>
+)
+Wrap App with PasswordGate for access control
+
